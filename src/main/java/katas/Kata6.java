b9c87f18caa4d17c62c520b4c2b369c6121da9a5
David <depch47@gmail.com>
2022-05-25 18:44:14 -0500

    Excersise 6 done

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -14,6 +14,11 @@ public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return "someUrl";
+        return movies.stream()
+                .map(movie -> movie.getBoxarts())
+                .flatMap(boxArts -> boxArts.stream())
+                .reduce((a, b) -> a.getWidth() * a.getHeight() > b.getWidth() * b.getHeight() ? a : b)
+                .map(boxArt -> boxArt.getUrl())
+                .orElseThrow();
     }
 }
