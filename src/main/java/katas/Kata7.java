7ea0ad2d9cdfdb0e4f21bfbe778077210017f25d
David <depch47@gmail.com>
2022-05-25 19:18:57 -0500

    Excersise 7 done

--- src/main/java/katas/Kata7.java
+++ src/main/java/katas/Kata7.java
@@ -3,12 +3,15 @@ package katas;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
+import model.BoxArt;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the id, title, and smallest box art url for every video
@@ -19,6 +22,20 @@ public class Kata7 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
+
+        return movieLists.stream()
+                .map(movieList -> movieList.getVideos())
+                .flatMap(movies -> movies.stream())
+                .map(movie -> {
+                    BoxArt smallestBoxArt = movie.getBoxarts().stream()
+                            .reduce((boxArt, boxArt2) -> boxArt.getHeight() * boxArt.getWidth() > boxArt2.getWidth() * boxArt2.getHeight() ? boxArt2 : boxArt).orElseThrow();
+                    HashMap<String, String> mapResult = new HashMap<>();
+                    mapResult.put("id", movie.getId().toString());
+                    mapResult.put("title", movie.getTitle());
+                    mapResult.put("boxart", smallestBoxArt.getUrl());
+                    return mapResult;
+                }).collect(Collectors.toList());
+
+//        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
     }
 }
